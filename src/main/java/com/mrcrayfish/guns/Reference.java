28f90509dfcbe3b27b7042c12f38b0b08be05a88
MrCrayfish <mrcrayfish@hotmail.com>
2018-08-25 13:10:02 +0930

    Updated version

--- src/main/java/com/mrcrayfish/guns/Reference.java
+++ src/main/java/com/mrcrayfish/guns/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "cgm";
 	public static final String MOD_NAME = "MrCrayfish's Gun Mod";
-	public static final String MOD_VERSION = "0.6.0";
+	public static final String MOD_VERSION = "0.6.1";
 	public static final String MC_VERSION = "[1.12.2]";
 	public static final String DEPENDENCIES = "required-after:obfuscate@[0.2.1,)";
 	public static final String PROXY_CLIENT = "com.mrcrayfish.guns.proxy.ClientProxy";
