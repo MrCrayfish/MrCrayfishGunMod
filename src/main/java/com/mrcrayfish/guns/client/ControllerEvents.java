3b1df346df7bd002a14cb65a4d7c83af19387ab5
MrCrayfish <mrcrayfish@hotmail.com>
2019-03-26 16:25:49 +1030

    You can now reload/unload with controller

--- src/main/java/com/mrcrayfish/guns/client/ControllerEvents.java
+++ src/main/java/com/mrcrayfish/guns/client/ControllerEvents.java
@@ -1,18 +1,27 @@
 package com.mrcrayfish.guns.client;
 
+import com.mrcrayfish.controllable.Controllable;
 import com.mrcrayfish.controllable.client.Action;
 import com.mrcrayfish.controllable.client.Buttons;
+import com.mrcrayfish.controllable.client.Controller;
 import com.mrcrayfish.controllable.event.AvailableActionsEvent;
 import com.mrcrayfish.controllable.event.ControllerEvent;
+import com.mrcrayfish.guns.ItemStackUtil;
 import com.mrcrayfish.guns.MrCrayfishGunMod;
+import com.mrcrayfish.guns.client.event.ReloadHandler;
+import com.mrcrayfish.guns.event.CommonEvents;
 import com.mrcrayfish.guns.item.ItemGun;
 import com.mrcrayfish.guns.item.ItemScope;
+import com.mrcrayfish.guns.network.PacketHandler;
+import com.mrcrayfish.guns.network.message.MessageUnload;
 import com.mrcrayfish.guns.object.Gun;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 /**
  * Author: MrCrayfish
@@ -20,6 +29,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ControllerEvents
 {
     private boolean shooting = false;
+    private int reloadCounter = -1;
 
     @SubscribeEvent
     public void onButtonInput(ControllerEvent.ButtonInput event)
@@ -48,6 +58,16 @@ public class ControllerEvents
                     if(heldItem.getItem() instanceof ItemGun)
                     {
                         event.setCanceled(true);
+                    }
+                    break;
+                case Buttons.X:
+                    if(heldItem.getItem() instanceof ItemGun)
+                    {
+                        event.setCanceled(true);
+                        if(event.getState())
+                        {
+                            reloadCounter = 0;
+                        }
                     }
                     break;
             }
@@ -113,6 +133,13 @@ public class ControllerEvents
                 event.getActions().put(Buttons.LEFT_TRIGGER, new Action("Aim", Action.Side.RIGHT));
                 event.getActions().put(Buttons.RIGHT_TRIGGER, new Action("Shoot", Action.Side.RIGHT));
 
+                ItemGun itemGun = (ItemGun) heldItem.getItem();
+                NBTTagCompound tag = heldItem.getTagCompound();
+                if(tag != null && tag.getInteger("AmmoCount") < itemGun.getGun().general.maxAmmo)
+                {
+                    event.getActions().put(Buttons.X, new Action("Reload", Action.Side.LEFT));
+                }
+
                 ItemStack scope = Gun.getScope(heldItem);
                 if (scope != null && MrCrayfishGunMod.proxy.isZooming())
                 {
@@ -123,6 +150,50 @@ public class ControllerEvents
                     }
                 }
             }
+        }
+    }
+
+    @SubscribeEvent
+    public void onRender(TickEvent.RenderTickEvent event)
+    {
+        Controller controller = Controllable.getController();
+        if(controller == null)
+            return;
+
+        if(event.phase == TickEvent.Phase.END)
+            return;
+
+        Minecraft mc = Minecraft.getMinecraft();
+        EntityPlayer player = mc.player;
+        if(player == null)
+            return;
+
+        if(mc.currentScreen == null && reloadCounter != -1)
+        {
+            if(controller.getState().x)
+            {
+                reloadCounter++;
+            }
+        }
+
+        if(reloadCounter > 40)
+        {
+            ReloadHandler.setReloading(false);
+            PacketHandler.INSTANCE.sendToServer(new MessageUnload());
+            reloadCounter = -1;
+        }
+        else if(reloadCounter > 0 && !controller.getState().x)
+        {
+            if(!Minecraft.getMinecraft().player.getDataManager().get(CommonEvents.RELOADING))
+            {
+                ReloadHandler.setReloading(true);
+                ReloadHandler.reloadingSlot = Minecraft.getMinecraft().player.inventory.currentItem;
+            }
+            else
+            {
+                ReloadHandler.setReloading(false);
+            }
+            reloadCounter = -1;
         }
     }
 }
