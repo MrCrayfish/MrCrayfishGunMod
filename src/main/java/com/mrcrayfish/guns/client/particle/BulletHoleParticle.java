3ace8361d977e63995b29b5dbac2b73d04002210
MrCrayfish <mrcrayfish@hotmail.com>
2020-04-29 16:34:05 +0930

    Bullet hole particle now uses the hit block's texture

--- src/main/java/com/mrcrayfish/guns/client/particle/BulletHoleParticle.java
+++ src/main/java/com/mrcrayfish/guns/client/particle/BulletHoleParticle.java
@@ -3,12 +3,14 @@ package com.mrcrayfish.guns.client.particle;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.mrcrayfish.guns.GunMod;
 import com.mrcrayfish.guns.client.ClientHandler;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.IParticleRenderType;
 import net.minecraft.client.particle.SpriteTexturedParticle;
 import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.Quaternion;
 import net.minecraft.client.renderer.Vector3f;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.inventory.container.PlayerContainer;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
@@ -23,17 +25,66 @@ public class BulletHoleParticle extends SpriteTexturedParticle
 {
     private Direction direction;
     private BlockPos pos;
+    private int uOffset;
+    private int vOffset;
+    private float textureDensity;
 
     public BulletHoleParticle(World world, double x, double y, double z, Direction direction, BlockPos pos)
     {
         super(world, x, y, z);
-        this.setSprite(Minecraft.getInstance().getAtlasSpriteGetter(PlayerContainer.LOCATION_BLOCKS_TEXTURE).apply(GunMod.BULLET_HOLE_TEXTURE));
+        this.setSprite(this.getSprite(pos));
         this.direction = direction;
         this.pos = pos;
         this.maxAge = 200;
         this.canCollide = false;
         this.particleGravity = 0.0F;
-        this.particleScale = 0.04F;
+        this.particleScale = 0.05F;
+
+    }
+
+    @Override
+    protected void setSprite(TextureAtlasSprite sprite)
+    {
+        super.setSprite(sprite);
+        this.uOffset = this.rand.nextInt(16);
+        this.vOffset = this.rand.nextInt(16);
+        this.textureDensity = (sprite.getMaxU() - sprite.getMinU()) / 16.0F; //Assuming texture is a square
+    }
+
+    private TextureAtlasSprite getSprite(BlockPos pos)
+    {
+        Minecraft minecraft = Minecraft.getInstance();
+        World world = minecraft.world;
+        if(world != null)
+        {
+            BlockState state = world.getBlockState(pos);
+            return Minecraft.getInstance().getBlockRendererDispatcher().getBlockModelShapes().getTexture(state);
+        }
+        return Minecraft.getInstance().getAtlasSpriteGetter(PlayerContainer.LOCATION_BLOCKS_TEXTURE).apply(GunMod.BULLET_HOLE_TEXTURE);
+    }
+
+    @Override
+    protected float getMinU()
+    {
+        return this.sprite.getMinU() + this.uOffset * this.textureDensity;
+    }
+
+    @Override
+    protected float getMinV()
+    {
+        return this.sprite.getMinV() + this.vOffset * this.textureDensity;
+    }
+
+    @Override
+    protected float getMaxU()
+    {
+        return this.getMinU() + this.textureDensity;
+    }
+
+    @Override
+    protected float getMaxV()
+    {
+        return this.getMinV() + this.textureDensity;
     }
 
     @Override
@@ -74,7 +125,7 @@ public class BulletHoleParticle extends SpriteTexturedParticle
         float f8 = this.getMaxU();
         float f5 = this.getMinV();
         float f6 = this.getMaxV();
-        int j = this.getBrightnessForRender(partialTicks);
+        int j = 0;
         buffer.pos((double) points[0].getX(), (double) points[0].getY(), (double) points[0].getZ()).tex(f8, f6).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j).endVertex();
         buffer.pos((double) points[1].getX(), (double) points[1].getY(), (double) points[1].getZ()).tex(f8, f5).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j).endVertex();
         buffer.pos((double) points[2].getX(), (double) points[2].getY(), (double) points[2].getZ()).tex(f7, f5).color(this.particleRed, this.particleGreen, this.particleBlue, this.particleAlpha).lightmap(j).endVertex();
