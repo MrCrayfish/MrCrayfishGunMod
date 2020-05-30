b09b8bbbb0ca90c2a4970a538e2d7d11316a1805
MrCrayfish <mrcrayfish@hotmail.com>
2020-05-30 18:03:27 +0930

    Added pin sound when grenade can be released

--- src/main/java/com/mrcrayfish/guns/init/ModSounds.java
+++ src/main/java/com/mrcrayfish/guns/init/ModSounds.java
@@ -25,6 +25,7 @@ public class ModSounds
 	public static final RegistryObject<SoundEvent> ITEM_MINI_GUN_FIRE = register("item.mini_gun.fire");
 	public static final RegistryObject<SoundEvent> ITEM_MACHINE_PISTOL_FIRE = register("item.machine_pistol.fire");
 	public static final RegistryObject<SoundEvent> ITEM_MACHINE_PISTOL_SILENCED_FIRE = register("item.machine_pistol.silenced_fire");
+	public static final RegistryObject<SoundEvent> ITEM_GRENADE_PIN = register("item.grenade.pin");
 	public static final RegistryObject<SoundEvent> ENTITY_STUN_GRENADE_EXPLOSION = register("entity.stun_grenade.explosion");
 	public static final RegistryObject<SoundEvent> ENTITY_STUN_GRENADE_RING = register("entity.stun_grenade.ring");
 	public static final RegistryObject<SoundEvent> UI_WEAPON_ATTACH = register("ui.weapon.attach");
--- src/main/java/com/mrcrayfish/guns/item/GrenadeItem.java
+++ src/main/java/com/mrcrayfish/guns/item/GrenadeItem.java
@@ -1,6 +1,7 @@
 package com.mrcrayfish.guns.item;
 
 import com.mrcrayfish.guns.entity.EntityThrowableGrenade;
+import com.mrcrayfish.guns.init.ModSounds;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -9,6 +10,7 @@ import net.minecraft.item.UseAction;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 
 /**
@@ -34,6 +36,16 @@ public class GrenadeItem extends AmmoItem
     public int getUseDuration(ItemStack stack)
     {
         return this.maxCookTime;
+    }
+
+    @Override
+    public void onUsingTick(ItemStack stack, LivingEntity player, int count)
+    {
+        int duration = this.getUseDuration(stack) - count;
+        if(duration == 10)
+        {
+            player.world.playSound(player.getPosX(), player.getPosY(), player.getPosZ(), ModSounds.ITEM_GRENADE_PIN.get(), SoundCategory.PLAYERS, 1.0F, 1.0F, false);
+        }
     }
 
     @Override
@@ -75,7 +87,6 @@ public class GrenadeItem extends AmmoItem
             int duration = this.getUseDuration(stack) - timeLeft;
             if(duration >= 10)
             {
-                System.out.println(this.maxCookTime - duration);
                 PlayerEntity player = (PlayerEntity) entityLiving;
                 EntityThrowableGrenade grenade = this.create(worldIn, player, this.maxCookTime - duration);
                 grenade.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, Math.min(1.0F, duration / 20F), 1.0F);
