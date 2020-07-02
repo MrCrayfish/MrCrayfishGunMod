70cff063fa66a6e31a7f75b1de838140635e8d08
MrCrayfish <mrcrayfish@hotmail.com>
2020-07-02 12:03:12 +0930

    Removed old attachment recipe

--- src/main/java/com/mrcrayfish/guns/init/ModRecipeSerializers.java
+++ src/main/java/com/mrcrayfish/guns/init/ModRecipeSerializers.java
@@ -2,7 +2,6 @@ package com.mrcrayfish.guns.init;
 
 import com.mrcrayfish.guns.Reference;
 import com.mrcrayfish.guns.crafting.WorkbenchRecipeSerializer;
-import com.mrcrayfish.guns.recipe.AttachmentRecipe;
 import com.mrcrayfish.guns.recipe.DyeItemRecipe;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.SpecialRecipeSerializer;
@@ -17,7 +16,6 @@ public class ModRecipeSerializers
 {
     public static final DeferredRegister<IRecipeSerializer<?>> REGISTER = new DeferredRegister<>(ForgeRegistries.RECIPE_SERIALIZERS, Reference.MOD_ID);
 
-    public static final RegistryObject<SpecialRecipeSerializer<AttachmentRecipe>> ATTACHMENT = REGISTER.register("attachment", () -> new SpecialRecipeSerializer<>(AttachmentRecipe::new));
     public static final RegistryObject<SpecialRecipeSerializer<DyeItemRecipe>> DYE_ITEM = REGISTER.register("dye_item", () -> new SpecialRecipeSerializer<>(DyeItemRecipe::new));
     public static final RegistryObject<WorkbenchRecipeSerializer> WORKBENCH = REGISTER.register("workbench", WorkbenchRecipeSerializer::new);
 }
