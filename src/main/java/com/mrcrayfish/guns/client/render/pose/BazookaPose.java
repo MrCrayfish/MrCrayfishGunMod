bdcd943d1830ac0fefdbc23497d997abe03f794e
MrCrayfish <mrcrayfish@hotmail.com>
2020-12-29 22:52:49 +1030

    Improved pose when holding bazooka

--- src/main/java/com/mrcrayfish/guns/client/render/pose/BazookaPose.java
+++ src/main/java/com/mrcrayfish/guns/client/render/pose/BazookaPose.java
@@ -1,44 +1,41 @@
 package com.mrcrayfish.guns.client.render.pose;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mrcrayfish.guns.client.render.HeldAnimation;
+import com.mrcrayfish.guns.client.AimPose;
+import com.mrcrayfish.guns.client.LimbPose;
 import com.mrcrayfish.guns.object.GripType;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.entity.model.PlayerModel;
-import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
-import net.minecraft.util.HandSide;
 
 /**
  * Author: MrCrayfish
  */
-public class BazookaPose implements HeldAnimation
+public class BazookaPose extends WeaponPose
 {
     @Override
-    public void applyPlayerModelRotation(PlayerEntity player, PlayerModel model, Hand hand, float aimProgress)
+    protected AimPose getUpPose()
     {
-        boolean right = Minecraft.getInstance().gameSettings.mainHand == HandSide.RIGHT ? hand == Hand.MAIN_HAND : hand == Hand.OFF_HAND;
-        ModelRenderer mainArm = right ? model.bipedRightArm : model.bipedLeftArm;
-        ModelRenderer secondaryArm = right ? model.bipedLeftArm : model.bipedRightArm;
-
-        mainArm.rotateAngleX = (float) Math.toRadians(-90F);
-        mainArm.rotateAngleY = (float) Math.toRadians(-35F) * (right ? 1F : -1F);
-        mainArm.rotateAngleZ = (float) Math.toRadians(0F);
-
-        secondaryArm.rotateAngleX = (float) Math.toRadians(-91F);
-        secondaryArm.rotateAngleY = (float) Math.toRadians(45F) * (right ? 1F : -1F);
-        secondaryArm.rotateAngleZ = (float) Math.toRadians(0F);
+        AimPose pose = new AimPose();
+        pose.getIdle().setRenderYawOffset(35F).setItemRotation(new Vector3f(10F, 0F, 0F)).setRightArm(new LimbPose().setRotationAngleX(-170F).setRotationAngleY(-35F).setRotationAngleZ(0F).setRotationPointY(4).setRotationPointZ(-2)).setLeftArm(new LimbPose().setRotationAngleX(-130F).setRotationAngleY(65F).setRotationAngleZ(0F).setRotationPointX(3).setRotationPointZ(1));
+        return pose;
     }
 
     @Override
-    public void applyPlayerPreRender(PlayerEntity player, Hand hand, float aimProgress, MatrixStack matrixStack, IRenderTypeBuffer buffer)
+    protected AimPose getForwardPose()
     {
-        boolean right = Minecraft.getInstance().gameSettings.mainHand == HandSide.RIGHT ? hand == Hand.MAIN_HAND : hand == Hand.OFF_HAND;
-        player.prevRenderYawOffset = player.prevRotationYaw + 35F * (right ? 1F : -1F);
-        player.renderYawOffset = player.rotationYaw + 35F * (right ? 1F : -1F);
+        AimPose pose = new AimPose();
+        pose.getIdle().setRenderYawOffset(35F).setRightArm(new LimbPose().setRotationAngleX(-90F).setRotationAngleY(-35F).setRotationAngleZ(0F).setRotationPointY(2).setRotationPointZ(0)).setLeftArm(new LimbPose().setRotationAngleX(-91F).setRotationAngleY(35F).setRotationAngleZ(0F).setRotationPointX(4).setRotationPointZ(0));
+        return pose;
+    }
+
+    @Override
+    protected AimPose getDownPose()
+    {
+        AimPose pose = new AimPose();
+        pose.getIdle().setRenderYawOffset(35F).setRightArm(new LimbPose().setRotationAngleX(-10F).setRotationAngleY(-35F).setRotationAngleZ(0F).setRotationPointY(2).setRotationPointZ(0)).setLeftArm(new LimbPose().setRotationAngleX(-10F).setRotationAngleY(15F).setRotationAngleZ(30F).setRotationPointX(4).setRotationPointZ(0));
+        return pose;
     }
 
     @Override
