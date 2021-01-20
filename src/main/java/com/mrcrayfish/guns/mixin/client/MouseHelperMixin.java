0a3b8fa3d3ebc98335b52f37504069fe57ce148a
MrCrayfish <mrcrayfish@hotmail.com>
2021-01-21 01:09:15 +1030

    Removed Hooks class

--- src/main/java/com/mrcrayfish/guns/mixin/client/MouseHelperMixin.java
+++ src/main/java/com/mrcrayfish/guns/mixin/client/MouseHelperMixin.java
@@ -1,7 +1,18 @@
 package com.mrcrayfish.guns.mixin.client;
 
+import com.mrcrayfish.guns.GunMod;
 import com.mrcrayfish.guns.client.Hooks;
+import com.mrcrayfish.guns.client.event.AimingHandler;
+import com.mrcrayfish.guns.init.ModSyncedDataKeys;
+import com.mrcrayfish.guns.item.GunItem;
+import com.mrcrayfish.guns.item.attachment.impl.Scope;
+import com.mrcrayfish.guns.object.Gun;
+import com.mrcrayfish.obfuscate.common.data.SyncedPlayerData;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHelper;
+import net.minecraft.client.settings.PointOfView;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,6 +27,30 @@ public class MouseHelperMixin
     @ModifyVariable(method = "updatePlayerLook()V", at = @At(value = "STORE", opcode = Opcodes.DSTORE), ordinal = 2)
     private double sensitivity(double original)
     {
-        return Hooks.applyModifiedSensitivity(original);
+        float additionalAdsSensitivity = 1.0F;
+        Minecraft mc = Minecraft.getInstance();
+        if(mc.player != null && !mc.player.getHeldItemMainhand().isEmpty() && mc.gameSettings.getPointOfView() == PointOfView.FIRST_PERSON)
+        {
+            ItemStack heldItem = mc.player.getHeldItemMainhand();
+            if(heldItem.getItem() instanceof GunItem)
+            {
+                GunItem gunItem = (GunItem) heldItem.getItem();
+                if(AimingHandler.get().isAiming() && !SyncedPlayerData.instance().get(mc.player, ModSyncedDataKeys.RELOADING))
+                {
+                    Gun modifiedGun = gunItem.getModifiedGun(heldItem);
+                    if(modifiedGun.getModules().getZoom() != null)
+                    {
+                        float newFov = modifiedGun.getModules().getZoom().getFovModifier();
+                        Scope scope = Gun.getScope(heldItem);
+                        if(scope != null)
+                        {
+                            newFov -= scope.getAdditionalZoom();
+                        }
+                        additionalAdsSensitivity = MathHelper.clamp(1.0F - (1.0F / newFov) / 10F, 0.0F, 1.0F);
+                    }
+                }
+            }
+        }
+        return original * (1.0 - (1.0 - GunMod.getOptions().getAdsSensitivity()) * AimingHandler.get().getNormalisedAdsProgress()) * additionalAdsSensitivity;
     }
 }
