26fdf6d814ea20f440a90d842a32f52ed0fb38a5
MrCrayfish <mrcrayfish@hotmail.com>
2021-05-09 04:07:57 +0930

    Cleaned up network code

--- src/main/java/com/mrcrayfish/guns/network/PacketHandler.java
+++ src/main/java/com/mrcrayfish/guns/network/PacketHandler.java
@@ -13,40 +13,26 @@ import java.util.function.Supplier;
 public class PacketHandler
 {
     public static final String PROTOCOL_VERSION = "1";
-    private static SimpleChannel handshakeChannel;
-    private static SimpleChannel playChannel;
+    private static final SimpleChannel HANDSHAKE_CHANNEL = NetworkRegistry.newSimpleChannel(new ResourceLocation(Reference.MOD_ID, "handshake"), () -> PROTOCOL_VERSION, s -> true, s -> true);
+    private static final SimpleChannel PLAY_CHANNEL = NetworkRegistry.newSimpleChannel(new ResourceLocation(Reference.MOD_ID, "play"), () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);;
     private static int nextMessageId = 0;
 
     public static void init()
     {
-        handshakeChannel = NetworkRegistry.ChannelBuilder
-                .named(new ResourceLocation(Reference.MOD_ID, "handshake"))
-                .networkProtocolVersion(() -> PROTOCOL_VERSION)
-                .clientAcceptedVersions(s -> true)
-                .serverAcceptedVersions(s -> true)
-                .simpleChannel();
-
-        handshakeChannel.messageBuilder(HandshakeMessages.C2SAcknowledge.class, 99)
+        HANDSHAKE_CHANNEL.messageBuilder(HandshakeMessages.C2SAcknowledge.class, 99)
                 .loginIndex(HandshakeMessages.LoginIndexedMessage::getLoginIndex, HandshakeMessages.LoginIndexedMessage::setLoginIndex)
                 .decoder(HandshakeMessages.C2SAcknowledge::decode)
                 .encoder(HandshakeMessages.C2SAcknowledge::encode)
                 .consumer(FMLHandshakeHandler.indexFirst((handler, msg, s) -> HandshakeHandler.handleAcknowledge(msg, s)))
                 .add();
 
-        handshakeChannel.messageBuilder(HandshakeMessages.S2CUpdateGuns.class, 1)
+        HANDSHAKE_CHANNEL.messageBuilder(HandshakeMessages.S2CUpdateGuns.class, 1)
                 .loginIndex(HandshakeMessages.LoginIndexedMessage::getLoginIndex, HandshakeMessages.LoginIndexedMessage::setLoginIndex)
                 .decoder(HandshakeMessages.S2CUpdateGuns::decode)
                 .encoder(HandshakeMessages.S2CUpdateGuns::encode)
                 .consumer(FMLHandshakeHandler.biConsumerFor((handler, msg, supplier) -> HandshakeHandler.handleUpdateGuns(msg, supplier)))
                 .markAsLoginPacket()
                 .add();
-
-        playChannel = NetworkRegistry.ChannelBuilder
-                .named(new ResourceLocation(Reference.MOD_ID, "play"))
-                .networkProtocolVersion(() -> PROTOCOL_VERSION)
-                .clientAcceptedVersions(PROTOCOL_VERSION::equals)
-                .serverAcceptedVersions(PROTOCOL_VERSION::equals)
-                .simpleChannel();
 
         registerPlayMessage(MessageAim.class, MessageAim::new, LogicalSide.SERVER);
         registerPlayMessage(MessageReload.class, MessageReload::new, LogicalSide.SERVER);
@@ -75,7 +61,7 @@ public class PacketHandler
      */
     private static <T extends IMessage> void registerPlayMessage(Class<T> clazz, Supplier<T> messageSupplier, LogicalSide side)
     {
-        playChannel.registerMessage(nextMessageId++, clazz, IMessage::encode, buffer -> {
+        PLAY_CHANNEL.registerMessage(nextMessageId++, clazz, IMessage::encode, buffer -> {
             T t = messageSupplier.get();
             t.decode(buffer);
             return t;
@@ -91,7 +77,7 @@ public class PacketHandler
      */
     public static SimpleChannel getHandshakeChannel()
     {
-        return handshakeChannel;
+        return HANDSHAKE_CHANNEL;
     }
 
     /**
@@ -99,6 +85,6 @@ public class PacketHandler
      */
     public static SimpleChannel getPlayChannel()
     {
-        return playChannel;
+        return PLAY_CHANNEL;
     }
 }
