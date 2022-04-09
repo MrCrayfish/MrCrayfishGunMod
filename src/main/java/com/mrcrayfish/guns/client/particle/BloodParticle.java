106fbcde7d3809dbeff97faccf87f45f26bf5846
MrCrayfish <mrcrayfish@hotmail.com>
2022-04-09 15:00:12 +0930

    📦 Removed hardcoded blood particle colour

--- src/main/java/com/mrcrayfish/guns/client/particle/BloodParticle.java
+++ src/main/java/com/mrcrayfish/guns/client/particle/BloodParticle.java
@@ -26,7 +26,6 @@ public class BloodParticle extends SpriteTexturedParticle
     public BloodParticle(ClientWorld world, double x, double y, double z)
     {
         super(world, x, y, z, 0.1, 0.1, 0.1);
-        this.setColor(0.541F, 0.027F, 0.027F);
         this.gravity = 1.5F;
         this.quadSize = 0.0625F;
         this.lifetime = (int)(12.0F / (this.random.nextFloat() * 0.9F + 0.1F));
