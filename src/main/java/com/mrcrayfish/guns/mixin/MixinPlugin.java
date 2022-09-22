7209958a3f12e7ba88749c9ac064c3df1564f8bd
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-09-22 18:12:20 +1000

    🐛 Allow forge to show its mods not found screen when Framework isn't installed (#368)

---
+++ src/main/java/com/mrcrayfish/guns/mixin/MixinPlugin.java
@@ -0,0 +1,52 @@
+package com.mrcrayfish.guns.mixin;
+
+import com.mrcrayfish.framework.Framework;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+    private boolean isFrameworkInstalled;
+    @Override
+    public void onLoad(String mixinPackage) {
+        try {
+            Class.forName("com.mrcrayfish.framework.Framework", false, this.getClass().getClassLoader());
+            isFrameworkInstalled = true;
+        } catch (Exception e) {
+            isFrameworkInstalled = false;
+        }
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return isFrameworkInstalled; // this makes sure that forge's helpful mods not found screen shows up
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
