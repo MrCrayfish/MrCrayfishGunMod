d970f5695533e98be9673f3dfee9dc900cc60611
Clumsy Alien <85691268+ClumsyAlien@users.noreply.github.com>
2022-12-02 13:17:49 -0500

    Apply fixes

--- src/main/java/com/tac/guns/extra_events/TacShootingEvent.java
+++ src/main/java/com/tac/guns/extra_events/TacShootingEvent.java
@@ -1,32 +1,26 @@
 package com.tac.guns.extra_events;
 
-import java.util.Locale;
 
 import com.mrcrayfish.obfuscate.common.data.SyncedPlayerData;
-import com.tac.guns.init.ModSyncedDataKeys;
-import org.apache.commons.lang3.ArrayUtils;
-
-/**
- * This class will be used for all shooting events that I will utilise.
- * The gun mod provides 3 events for firing guns check out {@link com.mrcrayfish.guns.event.GunFireEvent} for what they are
- */
-
-
 import com.tac.guns.Config;
 import com.tac.guns.Reference;
 import com.tac.guns.common.Gun;
 import com.tac.guns.event.GunFireEvent;
+import com.tac.guns.init.ModSyncedDataKeys;
 import com.tac.guns.item.GunItem;
-
+import com.tac.guns.item.TransitionalTypes.TimelessGunItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.text.KeybindTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.util.Locale;
 
 /**
  * Author: ClumsyAlien
@@ -34,42 +28,15 @@ import net.minecraftforge.fml.common.Mod;
 
 
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class TacShootingEvent {
-
-    /*
-        A bit decent bit of extra code will be locked in external methods such as this, separating some of the standard and advanced
-        Functions, especially in order to keep it all clean and allow easy backtracking, however both functions may receive changes
-        For now as much of the work I can do will be kept externally such as with fire selection, and burst fire.
-        (In short this serves as a temporary test bed to keep development on new functions on course)
-    */
-
-    @SubscribeEvent
+public class TacShootingEvent
+{
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void preShoot(GunFireEvent.Pre event)
     {
-        // Our gun?
-        if(!(event.getStack().getItem() instanceof GunItem))
+        if(!(event.getStack().getItem() instanceof TimelessGunItem))
             return;
         HandleFireMode(event);
     }
-    /*@SubscribeEvent
-    public void onShootTick(GunFireEvent.Pre event)
-    {
-        Minecraft mc = Minecraft.getInstance();
-        if (mc.player == null)
-            return;
-        if (!(event.getPlayer().getHeldItemMainhand().getItem() instanceof MBPGunItem))
-            return;
-        if(mc.world.isRaining()) {
-            event.setCanceled(true);
-            event.getPlayer().sendStatusMessage(new TranslationTextComponent("info." + Reference.MOD_ID + ".gun_waterlock"), true);
-        }
-        if(mc.player.isInWater()) {
-            event.setCanceled(true);
-            event.getPlayer().sendStatusMessage(new TranslationTextComponent("info." + Reference.MOD_ID + ".gun_waterlock"), true);
-        }
-    }*/
-
-
     private static void HandleFireMode(GunFireEvent.Pre event)
     {
         ItemStack gunItem = event.getStack();
@@ -114,4 +81,5 @@ public class TacShootingEvent {
             }
         }
     }
+
 }
