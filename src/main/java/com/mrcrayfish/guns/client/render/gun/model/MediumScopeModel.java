18a5bbc4088d1b65bf021b37786aca7be9fb6e5d
MrCrayfish <mrcrayfish@hotmail.com>
2023-01-08 18:36:57 +1030

    📦 Restored classes for compat but deprecated

---
+++ src/main/java/com/mrcrayfish/guns/client/render/gun/model/MediumScopeModel.java
@@ -0,0 +1,141 @@
+package com.mrcrayfish.guns.client.render.gun.model;
+
+import com.mojang.blaze3d.platform.Window;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Matrix3f;
+import com.mojang.math.Matrix4f;
+import com.mrcrayfish.guns.Reference;
+import com.mrcrayfish.guns.client.GunModel;
+import com.mrcrayfish.guns.client.GunRenderType;
+import com.mrcrayfish.guns.client.handler.AimingHandler;
+import com.mrcrayfish.guns.client.render.gun.IOverrideModel;
+import com.mrcrayfish.guns.client.util.RenderUtil;
+import com.mrcrayfish.guns.util.OptifineHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+
+import javax.annotation.Nullable;
+
+/**
+ * Deprecated. This scope utilises the new scope rendering and doesn't need custom rendering.
+ * To upgrade, create a .cgmmeta file for your scope and customise the properties.
+ */
+@Deprecated(since = "1.3.0", forRemoval = true)
+public class MediumScopeModel implements IOverrideModel
+{
+    private static final ResourceLocation HOLO_RETICLE = new ResourceLocation(Reference.MOD_ID, "textures/effect/holo_reticle.png");
+    private static final ResourceLocation HOLO_RETICLE_GLOW = new ResourceLocation(Reference.MOD_ID, "textures/effect/holo_reticle_glow.png");
+    private static final ResourceLocation VIGNETTE = new ResourceLocation(Reference.MOD_ID, "textures/effect/scope_vignette.png");
+
+    @Override
+    public void render(float partialTicks, ItemTransforms.TransformType transformType, ItemStack stack, ItemStack parent, @Nullable LivingEntity entity, PoseStack poseStack, MultiBufferSource renderTypeBuffer, int light, int overlay)
+    {
+        if(OptifineHelper.isShadersEnabled())
+        {
+            double transition = 1.0 - Math.pow(1.0 - AimingHandler.get().getNormalisedAdsProgress(), 2);
+            double zScale = 0.05 + 0.95 * (1.0 - transition);
+            poseStack.scale(1.0F, 1.0F, (float) zScale);
+        }
+
+        BakedModel bakedModel = Minecraft.getInstance().getItemRenderer().getItemModelShaper().getItemModel(stack);
+        Minecraft.getInstance().getItemRenderer().render(stack, ItemTransforms.TransformType.NONE, false, poseStack, renderTypeBuffer, light, overlay, GunModel.wrap(bakedModel));
+
+        if(true) return;
+
+
+        if(transformType.firstPerson() && entity != null && entity.equals(Minecraft.getInstance().player))
+        {
+            if(entity.getMainArm() == HumanoidArm.LEFT)
+            {
+                poseStack.scale(-1, 1, 1);
+            }
+
+            float scopeSize = 1.325F;
+            float size = scopeSize / 16.0F;
+            float crop = 0.4F;
+            Minecraft mc = Minecraft.getInstance();
+            Window window = mc.getWindow();
+            float texU = ((window.getScreenWidth() - window.getScreenHeight() + window.getScreenHeight() * crop * 2.0F) / 2.0F) / window.getScreenWidth();
+
+            poseStack.pushPose();
+            {
+                Matrix4f matrix = poseStack.last().pose();
+                Matrix3f normal = poseStack.last().normal();
+
+                poseStack.translate(-size / 2, 0.06, 1.5 * 0.0625);
+
+                float color = (float) AimingHandler.get().getNormalisedAdsProgress() * 0.8F + 0.2F;
+
+                VertexConsumer builder;
+
+                if(!OptifineHelper.isShadersEnabled())
+                {
+                    builder = renderTypeBuffer.getBuffer(GunRenderType.getScreen());
+                    builder.vertex(matrix, 0, size, 0).color(color, color, color, 1.0F).uv(texU, 1.0F - crop).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                    builder.vertex(matrix, 0, 0, 0).color(color, color, color, 1.0F).uv(texU, crop).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                    builder.vertex(matrix, size, 0, 0).color(color, color, color, 1.0F).uv(1.0F - texU, crop).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                    builder.vertex(matrix, size, size, 0).color(color, color, color, 1.0F).uv(1.0F - texU, 1.0F - crop).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                }
+
+                poseStack.translate(0, 0, 0.0001);
+
+                if(!OptifineHelper.isShadersEnabled())
+                {
+                    builder = renderTypeBuffer.getBuffer(RenderType.entityTranslucent(VIGNETTE));
+                    builder.vertex(matrix, 0, size, 0).color(color, color, color, 1.0F).uv(0, 1).overlayCoords(overlay).uv2(light).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                    builder.vertex(matrix, 0, 0, 0).color(color, color, color, 1.0F).uv(0, 0).overlayCoords(overlay).uv2(light).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                    builder.vertex(matrix, size, 0, 0).color(color, color, color, 1.0F).uv(1, 0).overlayCoords(overlay).uv2(light).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                    builder.vertex(matrix, size, size, 0).color(color, color, color, 1.0F).uv(1, 1).overlayCoords(overlay).uv2(light).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                }
+
+                double invertProgress = (1.0 - AimingHandler.get().getNormalisedAdsProgress());
+                poseStack.translate(-0.04 * invertProgress, 0.01 * invertProgress, 0);
+
+                double scale = 8.0;
+                poseStack.translate(size / 2, size / 2, 0);
+                poseStack.translate(-(size / scale) / 2, -(size / scale) / 2, 0);
+                poseStack.translate(0, 0, 0.0001);
+
+                int reticleGlowColor = RenderUtil.getItemStackColor(stack, parent, 0);
+                CompoundTag tag = stack.getTag();
+                if(tag != null && tag.contains("ReticleColor", Tag.TAG_INT))
+                {
+                    reticleGlowColor = tag.getInt("ReticleColor");
+                }
+
+                float red = ((reticleGlowColor >> 16) & 0xFF) / 255F;
+                float green = ((reticleGlowColor >> 8) & 0xFF) / 255F;
+                float blue = ((reticleGlowColor >> 0) & 0xFF) / 255F;
+                float alpha = (float) (1.0F * AimingHandler.get().getNormalisedAdsProgress());
+
+                if(!OptifineHelper.isShadersEnabled())
+                {
+                    builder = renderTypeBuffer.getBuffer(RenderType.entityTranslucent(HOLO_RETICLE_GLOW));
+                    builder.vertex(matrix, 0, (float) (size / scale), 0).color(red, green, blue, alpha).uv(0.0F, 0.9375F).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                    builder.vertex(matrix, 0, 0, 0).color(red, green, blue, alpha).uv(0.0F, 0.0F).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                    builder.vertex(matrix, (float) (size / scale), 0, 0).color(red, green, blue, alpha).uv(0.9375F, 0.0F).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                    builder.vertex(matrix, (float) (size / scale), (float) (size / scale), 0).color(red, green, blue, alpha).uv(0.9375F, 0.9375F).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                }
+
+                alpha = (float) (0.75F * AimingHandler.get().getNormalisedAdsProgress());
+
+                builder = renderTypeBuffer.getBuffer(RenderType.entityTranslucent(HOLO_RETICLE));
+                builder.vertex(matrix, 0, (float) (size / scale), 0).color(1.0F, 1.0F, 1.0F, alpha).uv(0.0F, 0.9375F).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                builder.vertex(matrix, 0, 0, 0).color(1.0F, 1.0F, 1.0F, alpha).uv(0.0F, 0.0F).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                builder.vertex(matrix, (float) (size / scale), 0, 0).color(1.0F, 1.0F, 1.0F, alpha).uv(0.9375F, 0.0F).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+                builder.vertex(matrix, (float) (size / scale), (float) (size / scale), 0).color(1.0F, 1.0F, 1.0F, alpha).uv(0.9375F, 0.9375F).overlayCoords(overlay).uv2(15728880).normal(normal, 0.0F, 1.0F, 0.0F).endVertex();
+            }
+            poseStack.popPose();
+        }
+    }
+}
