c8d57f06e5cbbf5d1b1d75b61a14d336c61aba61
MrCrayfish <mrcrayfish@hotmail.com>
2023-01-09 19:51:39 +1030

    📦 Added more fragile blocks to the default list

--- src/main/java/com/mrcrayfish/guns/datagen/BlockTagGen.java
+++ src/main/java/com/mrcrayfish/guns/datagen/BlockTagGen.java
@@ -24,14 +24,24 @@ public class BlockTagGen extends BlockTagsProvider
                 .addTag(Tags.Blocks.GLASS)
                 .addTag(BlockTags.CANDLES)
                 .add(Blocks.LILY_PAD)
-                .add(Blocks.BIG_DRIPLEAF)
-                .add(Blocks.BIG_DRIPLEAF_STEM)
                 .add(Blocks.COCOA)
                 .add(Blocks.END_ROD)
                 .add(Blocks.SCAFFOLDING)
                 .add(Blocks.SEA_PICKLE)
                 .add(Blocks.TURTLE_EGG)
                 .add(Blocks.GLOWSTONE)
-                .add(Blocks.SEA_LANTERN);
+                .add(Blocks.SEA_LANTERN)
+                .add(Blocks.ICE)
+                .add(Blocks.BLUE_ICE)
+                .add(Blocks.FROSTED_ICE)
+                .add(Blocks.PACKED_ICE)
+                .add(Blocks.BAMBOO)
+                .add(Blocks.SMALL_AMETHYST_BUD)
+                .add(Blocks.MEDIUM_AMETHYST_BUD)
+                .add(Blocks.LARGE_AMETHYST_BUD)
+                .add(Blocks.AMETHYST_CLUSTER)
+                .add(Blocks.LANTERN)
+                .add(Blocks.SOUL_LANTERN)
+                .add(Blocks.POINTED_DRIPSTONE);
     }
 }
