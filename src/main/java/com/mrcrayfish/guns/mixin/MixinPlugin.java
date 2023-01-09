b08352c20d7f6950809939d8f688d1900eec9ccb
MrCrayfish <mrcrayfish@hotmail.com>
2023-01-10 00:09:36 +1030

    📦 Fixed formatting

--- src/main/java/com/mrcrayfish/guns/mixin/MixinPlugin.java
+++ src/main/java/com/mrcrayfish/guns/mixin/MixinPlugin.java
@@ -1,6 +1,5 @@
 package com.mrcrayfish.guns.mixin;
 
-import com.mrcrayfish.framework.Framework;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -8,45 +7,48 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import java.util.List;
 import java.util.Set;
 
-public class MixinPlugin implements IMixinConfigPlugin {
+public class MixinPlugin implements IMixinConfigPlugin
+{
     private boolean isFrameworkInstalled;
+
     @Override
-    public void onLoad(String mixinPackage) {
-        try {
+    public void onLoad(String mixinPackage)
+    {
+        try
+        {
             Class.forName("com.mrcrayfish.framework.Framework", false, this.getClass().getClassLoader());
-            isFrameworkInstalled = true;
-        } catch (Exception e) {
-            isFrameworkInstalled = false;
+            this.isFrameworkInstalled = true;
+        }
+        catch(Exception e)
+        {
+            this.isFrameworkInstalled = false;
         }
     }
 
     @Override
-    public String getRefMapperConfig() {
+    public String getRefMapperConfig()
+    {
         return null;
     }
 
     @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        return isFrameworkInstalled; // this makes sure that forge's helpful mods not found screen shows up
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName)
+    {
+        return this.isFrameworkInstalled; // this makes sure that forge's helpful mods not found screen shows up
     }
 
     @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-
-    }
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
 
     @Override
-    public List<String> getMixins() {
+    public List<String> getMixins()
+    {
         return null;
     }
 
     @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
-    }
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
 
     @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
-    }
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
 }
