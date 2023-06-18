09551761b7814e56a31e52e772934063c621da73
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-06-18 21:51:01 +0930

    📦 Change item layer mixin to parent class

--- src/main/java/com/mrcrayfish/guns/mixin/client/ItemInHandLayerMixin.java
+++ src/main/java/com/mrcrayfish/guns/mixin/client/ItemInHandLayerMixin.java
@@ -8,9 +8,10 @@ import com.mrcrayfish.guns.common.Gun;
 import com.mrcrayfish.guns.item.GunItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.client.renderer.entity.layers.PlayerItemInHandLayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -24,38 +25,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 /**
  * Author: MrCrayfish
  */
-@Mixin(PlayerItemInHandLayer.class)
-public class PlayerItemInHandLayerMixin
+@Mixin(ItemInHandLayer.class)
+public class ItemInHandLayerMixin
 {
     @SuppressWarnings("ConstantConditions")
     @Inject(method = "renderArmWithItem", at = @At(value = "HEAD"), cancellable = true)
     private void renderArmWithItemHead(LivingEntity entity, ItemStack stack, ItemDisplayContext display, HumanoidArm arm, PoseStack poseStack, MultiBufferSource source, int light, CallbackInfo ci)
     {
-        InteractionHand hand = Minecraft.getInstance().options.mainHand().get() == arm ? InteractionHand.MAIN_HAND : InteractionHand.OFF_HAND;
-        if(hand == InteractionHand.OFF_HAND)
+        if(entity.getType() == EntityType.PLAYER)
         {
-            if(stack.getItem() instanceof GunItem)
+            InteractionHand hand = Minecraft.getInstance().options.mainHand().get() == arm ? InteractionHand.MAIN_HAND : InteractionHand.OFF_HAND;
+            if(hand == InteractionHand.OFF_HAND)
             {
-                ci.cancel();
-                return;
-            }
-
-            if(entity.getMainHandItem().getItem() instanceof GunItem gunItem)
-            {
-                Gun modifiedGun = gunItem.getModifiedGun(entity.getMainHandItem());
-                if(!modifiedGun.getGeneral().getGripType().getHeldAnimation().canRenderOffhandItem())
+                if(stack.getItem() instanceof GunItem)
                 {
                     ci.cancel();
                     return;
                 }
-            }
-        }
 
-        if(stack.getItem() instanceof GunItem gunItem)
-        {
-            ci.cancel();
-            PlayerItemInHandLayer<?, ?> layer = (PlayerItemInHandLayer<?, ?>) (Object) this;
-            renderArmWithGun(layer, (Player) entity, stack, gunItem, display, hand, arm, poseStack, source, light, Minecraft.getInstance().getFrameTime());
+                if(entity.getMainHandItem().getItem() instanceof GunItem gunItem)
+                {
+                    Gun modifiedGun = gunItem.getModifiedGun(entity.getMainHandItem());
+                    if(!modifiedGun.getGeneral().getGripType().getHeldAnimation().canRenderOffhandItem())
+                    {
+                        ci.cancel();
+                        return;
+                    }
+                }
+            }
+            if(stack.getItem() instanceof GunItem gunItem)
+            {
+                ci.cancel();
+                PlayerItemInHandLayer<?, ?> layer = (PlayerItemInHandLayer<?, ?>) (Object) this;
+                renderArmWithGun(layer, (Player) entity, stack, gunItem, display, hand, arm, poseStack, source, light, Minecraft.getInstance().getFrameTime());
+            }
         }
     }
 
